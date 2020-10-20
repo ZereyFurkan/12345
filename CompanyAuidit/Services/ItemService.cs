8090df4ee25b385657aae80c572057be3a932ea5
AslanAmca <aliaslan_61@outlook.com>
2020-10-20 23:28:03 +0300

    servis classları eklendi.

---
+++ CompanyAuidit/Services/ItemService.cs
@@ -0,0 +1,48 @@
+﻿using CompanyAuidit.Data;
+using CompanyAuidit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyAuidit.Services
+{
+    public class ItemService
+    {
+        private readonly CompanyAuiditContext companyAuiditContext;
+
+        public ItemService(CompanyAuiditContext companyAuiditContext)
+        {
+            this.companyAuiditContext = companyAuiditContext;
+        }
+
+        public List<Item> GetAll()
+        {
+            return companyAuiditContext.Items.ToList();
+        }
+
+        public Item GetById(int itemId)
+        {
+            return companyAuiditContext.Items.FirstOrDefault(item => item.Id == itemId);
+        }
+
+        public void Add(Item item)
+        {
+            companyAuiditContext.Items.Add(item);
+            companyAuiditContext.SaveChanges();
+        }
+
+        public void Update(Item item)
+        {
+            companyAuiditContext.Items.Update(item);
+            companyAuiditContext.SaveChanges();
+        }
+
+        public void Delete(int itemId)
+        {
+            var item = GetById(itemId);
+            companyAuiditContext.Items.Remove(item);
+            companyAuiditContext.SaveChanges();
+        }
+    }
+}
